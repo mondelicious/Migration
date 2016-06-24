ee0b20111bff818c799b3c0665df0b33bc7d242d
Richmond B. Lacanlalay <xednom@gmail.com>
2016-06-24 13:37:46 +0845

    Revised the connectionString

--- Migration/Program.cs
+++ Migration/Program.cs
@@ -15,9 +15,11 @@ namespace Migration
             checkDB();
             createDB();
             createTBL();
+            createEmployee();
             insertDT();
         }
 
+        //check database command
         public static void checkDB()
         {
             try
@@ -50,8 +52,32 @@ namespace Migration
 
             }
         }
-        
 
+        //create database command
+        public static void createDB()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("Server=localhost; port=3306;Uid=root;Pwd=;");
+
+                connection.Open();
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connection;
+                command.CommandText = "CREATE DATABASE db_pldt";
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Successfully created the database!");
+                Console.ReadLine();
+                connection.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot Create Database or the database already exist?", "System");
+                Console.ReadLine();
+            }
+        }
+
+        //create table command
         public static void createTBL()
         {
             MySqlConnection conn = new MySqlConnection("Server=localhost; Database=db_pldt; Uid=root; Pwd=;");
@@ -98,29 +124,8 @@ namespace Migration
             conn.Close();
         }
 
-        public static void createDB()
-        {
-            try
-            {
-                MySqlConnection connection = new MySqlConnection("Server=localhost; port=3306;Uid=root;Pwd=;");
-
-                connection.Open();
-                MySqlCommand command = new MySqlCommand();
-                command.Connection = connection;
-                command.CommandText = "CREATE DATABASE db_pldt";
-                command.ExecuteNonQuery();
-
-                Console.WriteLine("Successfully created the database!");
-                Console.ReadLine();
-                connection.Close();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Cannot Create Database or the database already exist?", "System");
-                Console.ReadLine();
-            }
-        }
-
+       
+        //insert actual data to the table
         public static void insertDT()
         {
             try
@@ -142,30 +147,31 @@ namespace Migration
                 Console.WriteLine("Cannot add data to the table.", "System");
                 Console.ReadLine();
             }
+}
+
+        //creating table tbl_employee
+        public static void createEmployee()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("Server=localhost;Database=db_pldt; port=3306; Uid=root; Pwd=;");
+
+                connection.Open();
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connection;
+                command.CommandText = "CREATE TABLE tbl_employee(Telephone_number VARCHAR(250), Service_Order VARCHAR(250), Type_of_Service VARCHAR(250), Time VARCHAR(250), Clrm_dispatch VARCHAR(250), Ok_no INT(11), Primary_cable VARCHAR(250), Secondary_cable VARCHAR(250), No_of_Span VARCHAR(250), Instrument_Modem VARCHAR(250), Paralleled_wire VARCHAR(250), Jacketed_wire VARCHAR(250), Protector VARCHAR(250), P_clamp VARCHAR(250), Ground_rod VARCHAR(250), Ground_wire VARCHAR(250), Connecting_block VARCHAR(250), Splitter VARCHAR(250), Subscriber_name VARCHAR(250), Address VARCHAR(250), Remarks VARCHAR(250), Installer_technician VARCHAR(250), Immediate_supervisor VARCHAR(250), Csoz_supervisor VARCHAR(250), Customer_service_Operation_zone VARCHAR(250), Submitted_by VARCHAR(250), Noted_by VARCHAR(250), Received_by VARCHAR(250))";
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Successfully created the Table");
+                Console.ReadLine();
+                connection.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot Create table it's either existing already or database is not connected");
+                Console.ReadLine();
+            }
         }
-
-        //public static void createEmployee()
-        //{
-        //    try
-        //    {
-        //        MySqlConnection connection = new MySqlConnection("Server=localhost; port=3306; Uid=root; Pwd=pisokins22;");
-
-        //        connection.Open();
-        //        MySqlCommand command = new MySqlCommand();
-        //        command.Connection = connection;
-        //        command.CommandText = "USE db_pldt CREATE TABLE tbl_employee()";
-        //        command.ExecuteNonQuery();
-
-        //        Console.WriteLine("Successfully created the Table");
-        //        Console.ReadLine();
-        //        connection.Close();
-        //    }
-        //    catch (Exception)
-        //    {
-        //        Console.WriteLine("Cannot Create table it's either existing already or database is not connected");
-        //        Console.ReadLine();
-        //    }
-        //}
 
     }
 }
